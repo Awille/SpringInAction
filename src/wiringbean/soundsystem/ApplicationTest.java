1a4d03e36bf7ef39957b63078bbcc3358156a232
will <Awille@qq.com>
2019-04-23 22:20:20 +0800

    从xml中加载配置

--- src/wiringbean/soundsystem/ApplicationTest.java
+++ src/wiringbean/soundsystem/ApplicationTest.java
@@ -2,10 +2,14 @@ package wiringbean.soundsystem;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationTest {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(CDPlayerConfig.class);
+        //java配置类装填
+        //ApplicationContext context = new AnnotationConfigApplicationContext(CDPlayerConfig.class);
+        //xml文件装填
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
         SgtPeppers sgtPeppers = context.getBean(SgtPeppers.class);
         sgtPeppers.play();
     }
