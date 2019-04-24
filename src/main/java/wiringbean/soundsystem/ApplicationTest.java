436c350adf8a243a4872cd1d10094ed4ccf23f25
will <Awille@qq.com>
2019-04-24 21:05:29 +0800

    classpath:D:/IdeaProjects/SpringInAction/target/classes/

--- src/main/java/wiringbean/soundsystem/ApplicationTest.java
+++ src/main/java/wiringbean/soundsystem/ApplicationTest.java
@@ -1,5 +1,6 @@
 package wiringbean.soundsystem;
 
+import jdk.internal.util.EnvUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
@@ -10,6 +11,7 @@ public class ApplicationTest {
         //ApplicationContext context = new AnnotationConfigApplicationContext(CDPlayerConfig.class);
         //xml文件装填
         ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/spring-config.xml");
+        //System.out.println("classpath: " + Thread.currentThread().getContextClassLoader().getResource("").getPath());
         SgtPeppers sgtPeppers = context.getBean(SgtPeppers.class);
         sgtPeppers.play();
     }
