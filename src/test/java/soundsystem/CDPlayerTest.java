b959af604a5119d08f9112b367bdc20cc2a1a706
will <Awille@qq.com>
2019-04-24 21:22:39 +0800

    简单的单元测试

--- src/test/java/soundsystem/CDPlayerTest.java
+++ src/test/java/soundsystem/CDPlayerTest.java
@@ -1,16 +1,24 @@
-package wiringbean.soundsystem;
+package soundsystem;
 
-
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import wiringbean.soundsystem.CDPlayerConfig;
+import wiringbean.soundsystem.CompactDisc;
 
-
-
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = CDPlayerConfig.class)
 public class CDPlayerTest {
     @Autowired
     private CompactDisc cd;
+
+    @Test
+    public void cdShouldNotBeNull() {
+        Assert.assertNotNull(cd);
+    }
 
 
 }
